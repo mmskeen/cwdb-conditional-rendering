e7eb43b0e0f3bff4aebd7d574f1f127e8c521a71
Michael Skeen <mmskeen@gmail.com>
2020-03-25 22:21:50 -0600

    finished exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import Login from "./Login";
+
+const isLoggedIn = true;
+const currentTime = new Date(2020, 3, 25, 13).getHours();
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {isLoggedIn ? currentTime > 12 && <h1>Hello</h1> : <Login />}
     </div>
   );
 }
